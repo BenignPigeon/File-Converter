88acf15a84b6ebf88dfc3df6a146297eaad12113
BenignPigeon <85837722+BenignPigeon@users.noreply.github.com>
2025-03-17 14:12:27 +0000

    v1.1.0

--- dependencies/universal-parameters.bat
+++ dependencies/universal-parameters.bat
@@ -1,4 +1,4 @@
 :: Get the current directory of the batch file
 set "script_dir=%~dp0..\"
 set "config_file=%script_dir%file_converter_config.txt"
-set "programVersion=1.0.2"
+set "programVersion=1.1.0"
