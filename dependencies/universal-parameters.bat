dc7a74b8c91333812f9b0b92aa2c1a34840436c5
BenignPigeon <85837722+BenignPigeon@users.noreply.github.com>
2025-04-09 17:18:46 +0100

    1.2.4

--- dependencies/universal-parameters.bat
+++ dependencies/universal-parameters.bat
@@ -1,7 +1,7 @@
 cd ..
 set "script_dir=%CD%"
 set "config_file=%APPDATA%\Bat-Files\File-Converter\file_converter_config.txt"
-set "programVersion=1.2.3"
+set "programVersion=1.2.4"
 set "programUpdater=https://gist.githubusercontent.com/BenignPigeon/67e6c94c9d73ec09fae74d3f3e28c468/raw/28ceda4def8543dac25e08cb296bd86ef117e259/install-youtube-downloader.ps1"
 
 exit /b
